66dd0fb5bdcc02b2c64cb1f7edd2964cd9be585f
Scott Shaffer <scottwshaffer1@gmail.com>
2022-09-02 14:20:02 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Showcase from './Showcase';
-import BaseStats from './BaseStats';
-import './App.css';
+import Showcase from "./Showcase";
+import BaseStats from "./BaseStats";
+import PokeMoves from "./PokeMoves";
+
+import "./App.css";
 
 function App() {
   const baseStats = {
@@ -19,9 +21,10 @@ function App() {
   };
 
   return (
-    <div className='main-wrapper background'>
+    <div className="main-wrapper background">
       <Showcase />
       <BaseStats clicker={handleClick} stats={baseStats} />
+      <PokeMoves />
     </div>
   );
 }
